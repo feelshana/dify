44f1f54743bc550b93f09e7b8a49d217eb534c4e
yanglolikong <xxx@xxx.com>
2025-07-08 11:25:55 +0800

    还原该页代码

--- web/app/components/share/utils.ts
+++ web/app/components/share/utils.ts
@@ -12,8 +12,7 @@ export const getInitialTokenV2 = (): Record<string, any> => ({
 
 export const checkOrSetAccessToken = async (appCode?: string) => {
   const sharedToken = appCode || globalThis.location.pathname.split('/').slice(-1)[0]
-  let userId = (await getProcessedSystemVariablesFromUrlParams()).user_id
-  userId = userId || localStorage.getItem('userName') as string
+  const userId = (await getProcessedSystemVariablesFromUrlParams()).user_id
   const accessToken = localStorage.getItem('dify_token') || JSON.stringify(getInitialTokenV2())
   let accessTokenJson = getInitialTokenV2()
   try {
