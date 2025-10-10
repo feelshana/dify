07b091b0b97eb641317feb333e5e3626fc38bf93
阳光 <shuanqverymuch@gmail.com>
2025-10-10 16:41:39 +0800

    fix: 给非分析云用户用的聊天框是从查询参数中去account

--- web/app/components/base/chat/chat-with-history/index.tsx
+++ web/app/components/base/chat/chat-with-history/index.tsx
@@ -58,11 +58,13 @@ const ChatWithHistory: FC<ChatWithHistoryProps> = ({
 
   const { t } = useTranslation()
   const searchParams = useSearchParams()
-  // #region 给非分析云用户用的聊天框是从查询参数中取的token
+  // #region 给非分析云用户用的聊天框是从查询参数中取的token, 从查询参数中去account
   const params = new URLSearchParams(searchParams)
   const elephant_token = params.get('token')
   elephant_token && localStorage.setItem('elephant_token', elephant_token)
-  // #endregion 给非分析云用户用的聊天框是从查询参数中取的token
+  const account = params.get('account')
+  account && localStorage.setItem('account', account)
+  // #endregion 给非分析云用户用的聊天框是从查询参数中取的token，从查询参数中去account
   const router = useRouter()
   const pathname = usePathname()
   const getSigninUrl = useCallback(() => {
