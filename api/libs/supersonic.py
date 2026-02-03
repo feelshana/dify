8e63506e2b17d02881f166b3d173da7960ebbebb
feelshana <151412598@qq.com>
2026-02-03 09:53:17 +0800

    增加红海token验证

--- api/libs/supersonic.py
+++ api/libs/supersonic.py
@@ -1,5 +1,11 @@
 from controllers.console.error import InvalidElephantTokenError
+from configs import dify_config
 from flask import request
+import requests
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def check_supersonic_token():
     # 检查是否存在X-ELEPHANT-TOKEN请求头
@@ -9,3 +15,24 @@ def check_supersonic_token():
         elephant_token = request.args.get("_elephant_token")
     if not elephant_token:
         raise InvalidElephantTokenError()
+    login_type = request.headers.get("loginType")
+    if login_type and login_type == 2:
+        logger.info("check redsea token")
+        check_redseatoken(elephant_token)
+
+def check_redseatoken(elephant_token):
+    if dify_config.REDSEA_TOKEN_URL:
+        response = requests.get(
+            dify_config.REDSEA_TOKEN_URL,
+            headers={"token": elephant_token},
+        )
+        logger.info("redsea token response: %s", response.text)
+        if response.status_code != 200:
+            raise InvalidElephantTokenError()
+        data = response.json()
+        if data.get("code") != 0:
+            raise InvalidElephantTokenError()
+        if not data.get("data"):
+            raise InvalidElephantTokenError()
+        if not data.get("data").get("oaAccount"):
+            raise InvalidElephantTokenError()
